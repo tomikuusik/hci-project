bad3038b1ab382a5324228385cdb9b51a7f7e75f
Tomi Kuusik <tomikuusik@gmail.com>
2022-05-30 22:25:18 +0300

    update

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  css: {
-    sass: {
-      additionalData: `@import "@/assets/global.scss"`
-    }
-  }
+  // css: {
+  //   sass: {
+  //     additionalData: `@import "@/assets/global.scss"`
+  //   }
+  // }
 });
